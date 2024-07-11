b95239c1f0ba515f2fba246e6fa23eba2b54e7be
fbk1996 <56232732+fbk1996@users.noreply.github.com>
2024-07-11 14:55:57 +0200

    added contact controller

---
+++ MechAppBackend/Controllers/ContactController.cs
@@ -0,0 +1,67 @@
+﻿using MechAppBackend.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace MechAppBackend.Controllers
+{
+    [Route("api/contact")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        /// <summary>
+        /// Sends a contact email with the provided message details.
+        /// </summary>
+        /// <param name="message">The contact message containing name, lastname, email, phone, and the message content.</param>
+        /// <returns>
+        /// A JSON result indicating the outcome of the operation.
+        /// Possible results include:
+        /// - "message_sended" if the email is successfully sent.
+        /// - "no_name" if the name is missing.
+        /// - "no_lastname" if the lastname is missing.
+        /// - "no_email" if the email is missing.
+        /// - "no_message" if the message content is missing.
+        /// - "error" if an error occurs during the operation.
+        /// </returns>
+        /// <remarks>
+        /// This endpoint does not require authentication.
+        /// </remarks>
+        [HttpPost]
+        public IActionResult SendEmail([FromBody]mailMessage message)
+        {
+            StringBuilder resultBuilder = new StringBuilder();
+
+            if (string.IsNullOrEmpty(message.name))
+                return new JsonResult(new { result = "no_name" });
+            if (string.IsNullOrEmpty(message.lastname))
+                return new JsonResult(new { result = "no_lastname" });
+            if (string.IsNullOrEmpty(message.email))
+                return new JsonResult(new { result = "no_email" });
+            if (string.IsNullOrEmpty(message.message))
+                return new JsonResult(new { result = "no_message" });
+
+            try
+            {
+                Sender.SendContactMessage("Wiadomość - kontakt", message.name, message.lastname, message.message, message.email, message.phone);
+                Sender.SendMailConfirmationtEmail("Wiadomość - kontakt", message.name, message.lastname, message.email, message.message);
+                resultBuilder.Append("message_sended");
+            }
+            catch (Exception ex)
+            {
+                resultBuilder.Clear().Append("error");
+                Logger.SendNormalException("MechApp", "ContactController", "SendEmail", ex);
+            }
+
+            return new JsonResult(new {result =  resultBuilder.ToString()});
+        }
+    }
+
+    public class mailMessage
+    {
+        public string? name { get; set; }
+        public string? lastname { get; set; }
+        public string? email { get; set; }
+        public string? phone { get; set; }
+        public string? message { get; set; }
+    }
+}
--- MechAppBackend/obj/Release/net6.0/MechAppBackend.AssemblyInfo.cs
+++ MechAppBackend/obj/Release/net6.0/MechAppBackend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MechAppBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e08e672345ba3310c348b946afdd006823184abf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c7bf65fe09466ebcd4689d19c34647ab33fc79f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MechAppBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MechAppBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
